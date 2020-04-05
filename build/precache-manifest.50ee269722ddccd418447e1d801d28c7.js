8320e2fe2fa99782a620ea224589bdbb5a36d216
Sarfaroj <sarfaroj@qilinlab.com>
2020-04-06 02:38:04 +0530

    new branch

---
+++ build/precache-manifest.50ee269722ddccd418447e1d801d28c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c7a5fa19b2d0509dc70c5e18a7462e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1e3f3aa9428c3a8ac5",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3acbac13ffd5531a295a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b1e3f3aa9428c3a8ac5",
+    "url": "/static/js/2.5a9b3b64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a9b3b64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3acbac13ffd5531a295a",
+    "url": "/static/js/main.1fd5435a.chunk.js"
+  },
+  {
+    "revision": "18101cbb0ecabe2f9782",
+    "url": "/static/js/runtime-main.3b9fe4d2.js"
+  }
+]);
